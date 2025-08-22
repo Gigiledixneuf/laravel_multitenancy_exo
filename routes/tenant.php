4b905f7ad8bfb130f0dc5c0c130e07c8f8c02354
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-08-22 10:50:59 +0100

    feat: configuration du package sactum pour utilisation des Api Rest dans le projet

--- routes/tenant.php
+++ routes/tenant.php
@@ -1,36 +1,20 @@
 <?php
 
-declare(strict_types=1);
-
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
-/*
-|--------------------------------------------------------------------------
-| Tenant Routes
-|--------------------------------------------------------------------------
-|
-| Here you can register the tenant routes for your application.
-| These routes are loaded by the TenantRouteServiceProvider.
-|
-| Feel free to customize them however you want. Good luck!
-|
-*/
-
 Route::middleware([
-    'web',
+    'api',
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
-])->group(function () {
-    Route::get('/', function () {
-        return 'Tenant ID: ' . tenant('id');
-    });
-    Route::get('/hello', function () {
-        dd(tenant());
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
-    });
-
+])->prefix('api')->group(function () {
+    Route::get('/hello', fn() => response()->json(['tenant_id' => tenant('id')]));
     Route::apiResource('/posts', PostController::class);
+
+    // Ceci est juste un example de comment utiliser le middleware Sanctum dans une route tenant
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::apiResource('/route_example_token', PostController::class);
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/central', function () {
+
+    return 'Bienvenue sur la partie centrale du SaaS.';
 });
